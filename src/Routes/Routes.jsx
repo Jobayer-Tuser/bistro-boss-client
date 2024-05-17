89fa816d18f0fbc5f3b64f590489df65c1265630
Hasanul-Alam <hasanulalam420@gmail.com>
2024-05-17 10:27:38 +0600

    pushing .env.local file to github

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -33,7 +33,7 @@ export const router = createBrowserRouter([
     // errorElement: <NotFound />,
     children: [
       {
-        path: '/',
+        index: true,
         element: <Home></Home>
       },
       {
